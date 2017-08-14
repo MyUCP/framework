85f4e0fac9083f9f7f42c1bc0136354ed296accd
maksa988 <maks.best5@mail.ru>
2017-08-14 18:50:26 +0300

    Initial commit

---
+++ src/Core.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace MyUCP;
+
+class Core {
+
+    /*
+     * MyUCP framework version
+     *
+     * @var string
+     */
+    const VERSION = '6.0.1';
+
+    /**
+     * The base path for the MyUCP installation.
+     *
+     * @var string
+     */
+    protected $basePath;
+
+
+}
