ee0cb03eb38a6f5841cdc90ea37bb4f122990914
maksa988 <maks.best5@mail.ru>
2017-08-14 18:53:32 +0300

    Update App Class

---
+++ src/App.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace MyUCP;
+
+class App {
+
+    /*
+     * MyUCP framework version
+     *
+     * @var string
+     */
+    const VERSION = '6.0.1';
+
+    /**
+     * The base path for the MyUCP installation.
+     *
+     * @var string
+     */
+    protected $basePath;
+
+    /**
+     * Create a new Illuminate application instance.
+     *
+     * @param  $basePath
+     * @return void
+     */
+    public function __construct($basePath = null)
+    {
+        if ($basePath) {
+            $this->setBasePath($basePath);
+        }
+    }
+
+    /**
+     * Get the version number of the application.
+     *
+     * @return string
+     */
+    public function version()
+    {
+        return static::VERSION;
+    }
+
+    /**
+     * Set the base path for the application.
+     *
+     * @param $basePath
+     * @return $this
+     */
+    public function setBasePath($basePath)
+    {
+        $this->basePath = rtrim($basePath, '\/');
+        return $this;
+    }
+
+}
