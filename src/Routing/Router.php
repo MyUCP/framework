139d1b35a610b66da898245fab04f75c45445137
maksa988 <maks.best5@mail.ru>
2017-08-15 03:43:07 +0300

    Router

---
+++ src/Routing/Router.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace MyUCP\Routing;
+
+class Router
+{
+    /**
+     * The route collection instance.
+     *
+     * @var \MyUCP\Routing\RouteCollection
+     */
+    protected $routes;
+
+    /**
+     * All of the verbs supported by the router.
+     *
+     * @var array
+     */
+    public static $verbs = ['GET', 'HEAD', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'];
+
+    /**
+     * Create a new Router instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->routes = new RouteCollection;
+    }
+
+    /**
+     * Register a new GET route with the router.
+     *
+     * @param  string  $uri
+     * @param  \Closure|array|string|null  $action
+     * @return \MyUCP\Routing\Route
+     */
+    public function get($uri, $action = null)
+    {
+        return $this->addRoute(['GET', 'HEAD'], $uri, $action);
+    }
+
+    /**
+     * Register a new POST route with the router.
+     *
+     * @param  string  $uri
+     * @param  \Closure|array|string|null  $action
+     * @return \MyUCP\Routing\Route
+     */
+    public function post($uri, $action = null)
+    {
+        return $this->addRoute('POST', $uri, $action);
+    }
+
+    /**
+     * Register a new PUT route with the router.
+     *
+     * @param  string  $uri
+     * @param  \Closure|array|string|null  $action
+     * @return \MyUCP\Routing\Route
+     */
+    public function put($uri, $action = null)
+    {
+        return $this->addRoute('PUT', $uri, $action);
+    }
+    /**
+     * Register a new PATCH route with the router.
+     *
+     * @param  string  $uri
+     * @param  \Closure|array|string|null  $action
+     * @return \MyUCP\Routing\Route
+     */
+
+    public function patch($uri, $action = null)
+    {
+        return $this->addRoute('PATCH', $uri, $action);
+    }
+
+    /**
+     * Register a new DELETE route with the router.
+     *
+     * @param  string  $uri
+     * @param  \Closure|array|string|null  $action
+     * @return \MyUCP\Routing\Route
+     */
+    public function delete($uri, $action = null)
+    {
+        return $this->addRoute('DELETE', $uri, $action);
+    }
+
+    /**
+     * Add a route to the underlying route collection.
+     *
+     * @param  array|string  $methods
+     * @param  string  $uri
+     * @param  \Closure|array|string|null  $action
+     * @return \MyUCP\Routing\Route
+     */
+    protected function addRoute($methods, $uri, $action)
+    {
+        return $this->routes->add($this->createRoute($methods, $uri, $action));
+    }
+    /**
+     * Create a new route instance.
+     *
+     * @param  array|string  $methods
+     * @param  string  $uri
+     * @param  mixed  $action
+     * @return \MyUCP\Routing\Route
+     */
+    protected function createRoute($methods, $uri, $action)
+    {
+        $route = $this->newRoute(
+            $methods, $uri, $action
+        );
+
+        return $route;
+    }
+
+    /**
+     * Create a new Route object.
+     *
+     * @param  array|string  $methods
+     * @param  string  $uri
+     * @param  mixed  $action
+     * @return \MyUCP\Routing\Route
+     */
+    protected function newRoute($methods, $uri, $action)
+    {
+        return (new Route($methods, $uri, $action))
+            ->setRouter($this);
+    }
+}
