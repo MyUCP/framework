eb5afffc60c441f3d2696a15c7ee4b31e1ad04e5
maksa988 <maks.best5@mail.ru>
2017-08-15 17:06:27 +0300

    App -> Core

--- src/Routing/Route.php
+++ src/Routing/Route.php
@@ -3,6 +3,7 @@
 namespace MyUCP\Routing;
 
 use LogicException;
+use MyUCP\Support\Arr;
 
 class Route
 {
@@ -222,15 +223,70 @@ class Route
         return $this;
     }
 
-    /**
-     * Dynamically access route parameters.
-     *
-     * @param  string  $key
-     * @return mixed
-     */
     public function __get($key)
     {
-        return $this->parameters[$key];
+        return $this->parameter($key);
+    }
+
+    /**
+     * Determine if the route has parameters.
+     *
+     * @return bool
+     */
+    public function hasParameters()
+    {
+        return isset($this->parameters);
+    }
+
+    /**
+     * Determine a given parameter exists from the route.
+     *
+     * @param  string $name
+     * @return bool
+     */
+    public function hasParameter($name)
+    {
+        if ($this->hasParameters()) {
+            return array_key_exists($name, $this->parameters());
+        }
+        return false;
+    }
+
+    /**
+     * Get a given parameter from the route.
+     *
+     * @param  string  $name
+     * @param  mixed   $default
+     * @return string|object
+     */
+    public function parameter($name, $default = null)
+    {
+        return Arr::get($this->parameters(), $name, $default);
+    }
+
+    /**
+     * Set a parameter to the given value.
+     *
+     * @param  string  $name
+     * @param  mixed   $value
+     * @return void
+     */
+    public function setParameter($name, $value)
+    {
+        $this->parameters();
+        $this->parameters[$name] = $value;
+    }
+
+    /**
+     * Unset a parameter on the route if it is set.
+     *
+     * @param  string  $name
+     * @return void
+     */
+    public function forgetParameter($name)
+    {
+        $this->parameters();
+        unset($this->parameters[$name]);
     }
 
     /**
